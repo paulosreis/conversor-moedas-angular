7c27e941b6749cca2a71c37f170f51c721aad85b
Paulo Guilherme S. Reis <paulosantos--@live.com>
2024-12-19 21:01:05 -0400

    feat: implementa componente de histórico de conversões com tratamento de erros e carregamento

--- src/app/components/historico/historico.component.ts
+++ src/app/components/historico/historico.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { MessageModule } from 'primeng/message';
+import { ExchangeService } from '../../services/exchange.service';
 
 @Component({
   selector: 'app-historico',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    TableModule,
+    ProgressSpinnerModule,
+    MessageModule,
+  ],
   templateUrl: './historico.component.html',
-  styleUrl: './historico.component.scss'
+  styleUrls: ['./historico.component.scss'],
 })
-export class HistoricoComponent {
+export class HistoricoComponent implements OnInit {
+  historico: any[] = [];
+  loading = true;
+  error: string | null = null;
 
+  constructor(private exchangeService: ExchangeService) { }
+
+  ngOnInit(): void {
+    this.exchangeService.getHistory().subscribe({
+      next: (response) => {
+        this.historico = response;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Erro ao carregar o histórico';
+        this.loading = false;
+      }
+    });
+  }
 }
