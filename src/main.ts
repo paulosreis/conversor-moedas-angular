542aa046856968a8c7feaab00319efdfd46587d4
Paulo Guilherme S. Reis <paulosantos--@live.com>
2024-12-19 21:25:45 -0400

    feat: adiciona navegação e configurações de rotas para a aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { ListagemComponent } from './app/components/listagem/listagem.component';
+import { ConversaoComponent } from './app/components/conversao/conversao.component';
+import { HistoricoComponent } from './app/components/historico/historico.component';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+const routes: Routes = [
+  { path: '', redirectTo: '/listagem', pathMatch: 'full' },
+  { path: 'listagem', component: ListagemComponent },
+  { path: 'conversao', component: ConversaoComponent },
+  { path: 'historico', component: HistoricoComponent }
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+}).catch(err => console.error(err));
